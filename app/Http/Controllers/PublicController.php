eb9e4475a4011396091204753b3074752ed5d2d7
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-05-03 10:42:59 +0300

    add posts with pagination

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
     public function index(){
-        $name = 'Kaspar';
-        return view('welcome', ['name' => $name]);
+        $posts = Post::simplePaginate(12);
+        return view('welcome', compact('posts'));
     }
     public function page1(){
         return view('page1');
